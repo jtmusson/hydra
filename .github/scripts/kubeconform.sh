6ffa4c2cbcf608a7f51359fe42733ff77525994c
James Musson <jtmusson@hey.com>
2026-03-09 10:52:54 +0000

    kubeconform: skip TalosUpgrade, KubernetesUpgrade

--- .github/scripts/kubeconform.sh
+++ .github/scripts/kubeconform.sh
@@ -12,7 +12,7 @@ kubeconform_args=(
     "-strict"
     "-ignore-missing-schemas"
     "-skip"
-    "Secret,ConfigMap"
+    "Secret,ConfigMap,tuppr.home-operations.com/v1alpha1/TalosUpgrade,tuppr.home-operations.com/v1alpha1/KubernetesUpgrade"
     "-schema-location"
     "default"
     "-schema-location"
